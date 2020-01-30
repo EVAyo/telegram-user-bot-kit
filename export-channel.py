f36b0cbe1efd3a6162ba32a11b1a0a6890a0d79e
Septs <github@septs.pw>
2020-01-30 16:28:31 +0800

    Regular update: 2020-01-30T08:28:31Z

---
+++ export-channel.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+import csv
+import time
+from datetime import datetime
+
+from pyrogram import Client, Message
+
+app = Client("my_account")
+
+
+def export_channel(chat_id: int):
+    fp = open("%s-channel.csv" % chat_id, "w", newline="")
+    writer = csv.writer(fp)
+    writer.writerow(["Date", "Message"])
+    for message in app.iter_history(chat_id=chat_id):
+        message: Message
+        if message.service or message.empty or message.media or message.via_bot:
+            continue
+        send_date = datetime.fromtimestamp(message.date).astimezone().isoformat()
+        writer.writerow([send_date, message.text])
+    fp.close()
+
+
+def main():
+    chat_id = int(input("Chat ID: "))
+    app.start()
+    export_channel(chat_id)
+    app.stop()
+
+
+if __name__ == "__main__":
+    main()
